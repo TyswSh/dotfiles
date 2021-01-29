18e6f753af43e00276fec344182da710f119de5d
TyswSh <amaleg77@gmail.com>
2021-01-30 04:37:26 +0900

    change mv xprofile

--- startup_installer.fish
+++ startup_installer.fish
@@ -9,18 +9,7 @@ yes | sudo pacman -Syy && sudo pacman -Syyu
 
 # 日本語入力環境
 yes | sudo pacman -S fcitx-im fcitx-mozc fcitx-configtool
-set ex 'export'
-set mo 'XMODIRIER'
-set addtions -e "\
-$ex LANG=\"ja_JP.UTF-8\"
-$ex $mo\S=\"@im=fcitx\"
-$ex $mo=\"@im=fcitx\"
-$ex GTK_IM_MODULE=fcitx
-$ex QT_IM_MODULE=fcitx
-$se DefaultIMModule=fcitx"
-
-set fileName '/.xprofile'
-echo $addtions >> $HOME$fileName
+mv .xprofile $HOME/
 
 # ディレクトリ名の変更
 yes | sudo pacman -S xdg-user-dirs-gtk
